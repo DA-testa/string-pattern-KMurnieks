18101ff6ccf8b218424aa7fea9e206686e4eac95
Kristaps Murnieks <118051283+KMurnieks@users.noreply.github.com>
2023-04-13 16:16:01 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -4,16 +4,16 @@
 def read_input():
     filepath = './tests/'
     file = '06'
-    folder = filepath + file
+    folder = filepath + file  #FILE
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
     input_type = input().rstrip()
 
-    if input_type == 'I':
+    if input_type == 'I': #INPUIT
         pattern = input().rstrip()
         text = input().rstrip()
     else:
-        with open(folder, 'r') as f:
+        with open(folder, 'r') as f:  #Otrs INPUT
             pattern = f.readline().rstrip()
             text = f.readline().rstrip()
 
@@ -35,11 +35,11 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     positions = []
-    pattern_len = len(pattern)
+    pattern_len = len(pattern)      #Define zzzzz
     text_len = len(text)
  
     par = 31
-    met = 10**9 + 9
+    met = 10**9 + 9   ##Define zzzz
     p_pow = [1]
     h = [0] * (text_len + 1)
     for i in range(1, text_len + 1):
@@ -47,7 +47,7 @@ def get_occurrences(pattern, text):
         h[i] = (h[i - 1] * par + ord(text[i - 1])) % met
 
     pattern_hash = 0
-    for i in range(pattern_len):
+    for i in range(pattern_len):   
         pattern_hash = (pattern_hash * par + ord(pattern[i])) % met
     for i in range(text_len - pattern_len + 1):
         if pattern_hash != (h[i + pattern_len] - h[i] * p_pow[pattern_len]) % met:
