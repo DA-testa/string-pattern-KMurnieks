4b1c32cdaf0b6278c7632f5eff80fe53842491c4
Kristaps Murnieks <118051283+KMurnieks@users.noreply.github.com>
2023-04-13 15:40:48 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -7,22 +7,24 @@ def read_input():
     folder = filepath + file
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    input_type = input().rstrip()
 
-    input_type = input("Enter 'F' to read from file, or press any other key to enter text manually: ")
-
-    if input_type == "F":
-        with open(folder) as f:
-            text = f.read().rstrip()
+    if input_type == 'I':
+        pattern = input().rstrip()
+        text = input().rstrip()
     else:
-        text = input("Enter the text: ").rstrip()
-    pattern = input("Enter the pattern: ").rstrip()
+        with open('folder', 'r') as f:
+            pattern = f.readline().rstrip()
+            text = f.readline().rstrip()
 
+    return pattern, text
+
+    
     
     # after input type choice
     # read two lines 
     # first line is pattern 
-    # second line is text in which to look for pattern 
-    return pattern, text
+
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
@@ -33,15 +35,26 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
+    prime = 1000000007
+    multiplier = 263
+    pattern_hash = 0
+    text_hash = 0
+    multiplier_pow = 1
+    positions = []
 
-    result = []
-    p_len, t_len = len(pattern), len(text)
-    p_hash = hash(pattern)
-    t_hashes = [hash(text[i:i+p_len]) for i in range(t_len - p_len + 1)]
-    for i, h in enumerate(t_hashes):
-        if h == p_hash and text[i:i+p_len] == pattern:
-            result.append(i)
-    return result
+    for i in range(len(pattern)):
+        pattern_hash = (pattern_hash + ord(pattern[i]) * multiplier_pow) % prime
+        text_hash = (text_hash + ord(text[i]) * multiplier_pow) % prime
+        multiplier_pow = (multiplier_pow * multiplier) % prime
+
+    for i in range(len(text) - len(pattern) + 1):
+        if pattern_hash == text_hash:
+            if text[i:i+len(pattern)] == pattern:
+                positions.append(i)
+        if i == len(text) - len(pattern):
+            break
+        text_hash = (text_hash - ord(text[i]) + ord(text[i+len(pattern)]) * multiplier_pow) % prime
+    return positions
 
 
 
